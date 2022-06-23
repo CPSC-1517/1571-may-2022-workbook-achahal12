39455d82c9dd3db198c62567d26b314e06fcbaaf
achahal12 <77082874+achahal12@users.noreply.github.com>
2022-06-22 18:05:28 -0600

    WebAppRezorPage

---
+++ CSRazorSolution/CSRazorSolution1/WebApp/Pages/Samples/BasicDataManagment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Samples
+{
+    public class BasicDataManagmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
